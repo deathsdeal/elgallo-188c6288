6c409df138025b945ac2b4e481fbf63f9d22393c
deathsdeal <32213538+deathsdeal@users.noreply.github.com>
2026-07-25 15:25:23 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,5 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
-
-    prerender: {
-      enabled: true,
-      autoStaticPathsDiscovery: true,
-      crawlLinks: true,
-      failOnError: true,
-    },
   },
 });
